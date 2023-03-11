6132ca6c9c30479d6c87790d9627c29ab3f52f31
4B1G0062 <4b1g0062@stust.edu.tw>
2023-03-11 16:18:47 +0800

    成功

--- No.2.cpp
+++ No.2.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
-
+#include <iomanip>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    float  mile = 0.0, kilometer=0.0;
+    cin >> mile;
+    kilometer = mile * 1.6;
+    cout << fixed << setprecision(1) << kilometer << endl;
+    return 0;
 }
