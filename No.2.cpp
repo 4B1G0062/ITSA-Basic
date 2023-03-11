e356fdd64b4f5048575015ddd8bc4dd6ca4726ff
4B1G0062 <4b1g0062@stust.edu.tw>
2023-03-11 15:42:36 +0800

    加入專案檔案。

---
+++ No.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    string a, b, temp = { 0 };
+
+    cin >> a;
+    cin >> b;
+    for (int i = 0; i < a.length() - b.length(); i++) {
+        for (int j = 0; j < b.length(); j++) {
+            temp[j] = b[i];
+        }
+        if (temp == a) {
+            count += 1;
+        }
+        temp = { 0 };
+    }
+    cout << count << endl;
+
+    return 0;
+}
