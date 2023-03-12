b673be5ea0a1cefc4fe65ca440fdd496fbd48510
4B1G0062 <4b1g0062@stust.edu.tw>
2023-03-12 13:05:19 +0800

    通過

--- No.2.cpp
+++ No.2.cpp
@@ -1,20 +1,35 @@
-﻿// No.2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
 
-#include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int month;
+
+    cin >> month;
+    switch (month) {
+        case 1:
+        case 2:
+            cout << "Winter" << endl;
+            break;
+        case 3:
+        case 4:
+        case 5:
+            cout << "Spring" << endl;
+            break;
+        case 6:
+        case 7:
+        case 8:
+            cout << "Summer" << endl;
+            break;
+        case 9:
+        case 10:
+        case 11:
+            cout << "Autumn" << endl;
+            break;
+        case 12:
+            cout << "Winter" << endl;
+            break;
+    }
+    
+    return 0;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
