0c9afd6fa462b8091dc6272691f115e473403a14
4B1G0062 <4b1g0062@stust.edu.tw>
2023-03-12 16:19:51 +0800

    新增註解

--- No.2.cpp
+++ No.2.cpp
@@ -5,15 +5,16 @@ int main()
 {
     int decimal, binary[8] = { 0 }, i;
 
-    cin >> decimal;
-    if (decimal < 0) {
+    cin >> decimal;  //輸入十進位
+
+    if (decimal < 0) {  //如果輸入為負數，用1的補數計算
         decimal += 256;
     }
-    for (i = 7; i >= 0; i--) {
+    for (i = 7; i >= 0; i--) {   //除2取餘，逆向排列法計算
         binary[i] = decimal % 2;
         decimal = decimal / 2;
     }
-    for (i = 0; i <= 7; i++) {
+    for (i = 0; i <= 7; i++) {   //顯示二進位結果
         cout << binary[i];
     }
     cout << endl;
