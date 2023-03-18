44f8f55a2ae379f52d70e227af97f3d85e3ed5b4
ZhangTing-Wei <a4990090020@gmail.com>
2023-03-18 21:40:09 +0800

    通過

--- No.2.cpp
+++ No.2.cpp
@@ -1,20 +1,68 @@
-﻿// No.2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
+#include <string>
+using namespace std;
 
-#include <iostream>
+class Calaulate {
+private:
+    int real, empty;
+public:
+    Calaulate(int r = 0, int e = 0) {
+        real = r;
+        empty = e;
+    }
+
+    Calaulate operator + (Calaulate const& obj) {
+        Calaulate result;
+        result.real = real + obj.real;
+        result.empty = empty + obj.empty;
+        return result;
+    }
+
+    Calaulate operator - (Calaulate const& obj) {
+        Calaulate result;
+        result.real = real - obj.real;
+        result.empty = empty - obj.empty;
+        return result;
+    }
+
+    Calaulate operator * (Calaulate const& obj) {
+        Calaulate result;
+        result.real = (real * obj.real - empty * obj.empty);
+        result.empty = (real * obj.empty + empty * obj.real);
+        return result;
+    }
+
+    void dispaly() {
+        cout << real << " " << empty << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, real_1, empty_1, real_2, empty_2, i;
+    char oper;
+
+    cin >> n;
+    Calaulate result[10] = { 0 };
+
+    for (i = 0; i < n; i++) {
+        cin >> oper >> real_1 >> empty_1 >> real_2 >> empty_2;
+        Calaulate a(real_1, empty_1);
+        Calaulate b(real_2, empty_2);
+
+        switch (oper) {
+            case '+':
+                result[i] = a + b;
+                break;
+            case '-':
+                result[i] = a - b;
+                break;
+            case '*':
+                result[i] = a * b;
+                break;
+        }
+        result[i].dispaly();
+    }
+
+    return 0;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
