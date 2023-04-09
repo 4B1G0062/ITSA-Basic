304cefdff544b830b00a547a469a2551f098ebe0
ZhangTing-Wei <a4990090020@gmail.com>
2023-04-09 15:02:01 +0800

    反正我看的輸出結果都正確，我真的不知道錯在哪裡👎

--- No.2.cpp
+++ No.2.cpp
@@ -5,26 +5,52 @@ using namespace std;
 
 int main()
 {
-    int i;
-    string input, temp;
+    int i, j;
+    string input, temp, output;
     vector<string> str;
 
     getline(cin, input);
 
-    for (i = 0; i < input.length(); i++) {
+    for (i = 0; i < input.length(); i++) {  //大寫轉小寫
         if (input[i] >= 'A' && input[i] <= 'Z') {
             input[i] = (char)(input[i] + 32);
         }
 
-        if (input[i + 1] != ' ') {
+        if (input[i] != ' ') {
             temp = temp + input[i];
         }
         else {
             str.push_back(temp);
+            temp = "";
         }
-        
     }
-    cout << input << endl;
+    str.push_back(temp);
+
+    for (i = str.size() - 1; i >= 0; i--) {  //比較是否一樣
+        for (j = 0; j < str.size(); j++) {
+            if (i == j) {
+                continue;
+            }
+            else {
+                if (str[i] == str[j]) {
+                        str[i] = "";
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < str.size(); i++) {  //輸出結果存入output，最後有空白" "
+        if (str[i] == "")
+            continue;
+        else {
+            output += str[i] + " ";
+        }
+    }
+
+    for (i = 0; i < output.length() - 1; i++) {  //輸出結果最後沒有空白" "
+        cout << output[i];
+    }
+    cout << endl;
 
     return 0;
 }
